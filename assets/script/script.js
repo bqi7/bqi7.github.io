7a0d3ce90585b23332be11c0a19cc7fc1ced4873
Brian Qi <bqi7@berkeley.edu>
2019-04-26 21:17:06 -0700

    Personal Website

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // fade in/out effect for occupation texts
+    function runSequence(i) {       
+        if (words.length > i) {
+            setTimeout(function() {
+                setTimeout('$("#occupations").fadeOut()', 1600);
+                document.getElementById("occupations").innerHTML = words[i];
+                $("#occupations").fadeIn(300);
+                runSequence(++i);
+            }, 2000); 
+
+        } else if (words.length == i) { 
+            runSequence(0);
+        }
+    }
+    var words = ['Computer Science Mentor', 'Software Engineer', 'Artist', 'American Kiwi', 'UC Berkeley Student'];   
+    setTimeout('$("#occupations").fadeOut()', 1500);
+    runSequence(0);
+
+    // change topbar after scrolling
+    $(function () {
+        $(document).scroll(function() {
+          var bar = $("#topbar");
+          bar.toggleClass('scrolled', ($(this).scrollTop() > 0.8*screen.height));
+        });
+    });
+});
