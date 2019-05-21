cce6c4017450c6fcf71de7b0ddd7dd43a13bf77b
Brian Qi <bqi7@berkeley.edu>
2019-05-21 01:45:20 -0700

    minor fixes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     // fade in/out effect for occupation texts
     function runSequence(i) {  
-        $("#display").stop(true, true);      
+//        $("#display").stop(true, true);      
         if (words.length > i) {
             setTimeout(function() {
                 setTimeout('$("#occupations").fadeOut()', 1600);
                 // document.getElementById("occupations").innerHTML = words[i];
                 $("#occupations").text(words[i]);
-                $("#occupations").fadeIn(300);
+                $("#occupations").fadeIn(400);
                 runSequence(++i);
             }, 2000); 
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
         }
     }
     var words = ['Computer Science Mentor', 'Software Engineer', 'Artist', 'American Kiwi', 'UC Berkeley Student'];   
-    setTimeout('$("#occupations").fadeOut()', 1500);
+    setTimeout('$("#occupations").fadeOut()', 1600);
     runSequence(0);
 
     // change topbar after scrolling
